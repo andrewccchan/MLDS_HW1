84cf41ec6e99e3c99eb3c31de3aefef91db911f3
SimpleA <b03901057@ntu.edu.tw>
2017-10-18 10:09:35 +0800

    cnn code first version (incomplete)

--- src/main.py
+++ src/main.py
@@ -4,31 +4,12 @@ import tensorflow as tf
 import utility
 import network
 from utility import BatchGenerator
-#TODO: Note that sklearn is used for non-training purpose in this code
 
 def split_train_test(data, test_ratio):
     data_len = len(data)
     test_len = int(data_len * test_ratio)
     np.random.shuffle(data)
     return (data[test_len:], data[:test_len])
-
-# Post-process phone buffer (remove leading and tailing 'sil' and aslo repetitive chars)
-def process_phone_buffer(phone_buffer):
-    processed = []
-    last_char = ''
-    # Remove repetitive chars
-    for phone in phone_buffer:
-        if phone != last_char:
-            processed.append(phone)
-        last_char = phone
-    # Remove leading and tailing 'sil'
-    if processed[0] == 'L':
-        del processed[0]
-    if processed[-1] == 'L':
-        del processed[-1]
-
-    return ''.join(processed)
-
 
 def eval_network(sess, test_speaker, graph, params, max_speaker=None):
     # print('Validating network...')
@@ -66,48 +47,6 @@ def eval_network(sess, test_speaker, graph, params, max_speaker=None):
         s.reset()
 
     print('Validation loss = {}'.format(total_loss / step_cnt))
-
-
-def predict(sess, X, graph, params, idx_phone_map,
-            phone_reduce_map, reduce_char_map, out_path):
-    batch_size = params['batch_size']
-    num_steps = params['num_steps']
-    out_file = open(out_path, 'w')
-    out_file.write('id,phone_sequence\n')
-    last_id = None
-    cur_id = None
-    phone_buffer = []
-    for idx, epoch in enumerate(utility.gen_epochs(X, y, 1, batch_size, num_steps)):
-        for step, (X_batch, y_batch) in enumerate(epoch):
-            # Perform predictions
-            actual_batch_size = len(X_batch)
-            dummy_y = np.zeros((actual_batch_size, params['num_steps']))
-            X_batch = np.asarray(X_batch)
-            feed_dict = {graph['x']:X_batch[:, :, 1:], graph['y']:dummy_y}
-            loss_, predict_ = sess.run([graph['total_loss'], \
-            graph['predictions']], feed_dict=feed_dict)
-
-            for idx in range(X_batch.shape[0]):
-                # Get speaker ID
-                phone_id = X_batch[idx, -1, 0]
-                cur_id = '_'.join(phone_id.split('_')[0:2])
-
-                # Get predicted char
-                predict_phone = idx_phone_map[predict_[idx]]
-                predict_phone_reduce = phone_reduce_map[predict_phone]
-                predict_char = reduce_char_map[predict_phone_reduce]
-
-                # Write out phone sequence of previous speaker
-                if cur_id != last_id and len(phone_buffer) != 0:
-                    out_file.write(last_id + ',' + process_phone_buffer(phone_buffer) + '\n')
-                    del phone_buffer[:]
-                last_id = cur_id
-                phone_buffer.append(predict_char)
-
-        # Write out phone sequence of the last speaker
-        out_file.write(last_id + ',' + process_phone_buffer(phone_buffer) + '\n')
-
-    out_file.close()
 
 def train_network(speaker_list, graph, params, model_path):
     batch_size = params['batch_size']
@@ -172,32 +111,29 @@ def train_network(speaker_list, graph, params, model_path):
 
 if __name__ == '__main__':
     params = dict(
-        state_size = 100,
+        feature_size = 39,
+        num_steps = 20,
         num_classes = 48,
-        batch_size = 128,
-        num_steps = 15,
-        # num_layer = 1
-        fea_num = 39,
+        cnn_filter_size = 3,
+        cnn_layer_num = 3,
+        cnn_filter_num = [32, 64, 64],
+        cnn_pool_size = [2, 1, 1],
+        rnn_state_size = 100,
         learning_rate = 1e-4,
-        num_epochs = 50)
+        num_epochs = 50
+    )
 
     (phone_idx_map, idx_phone_map, idx_char_map, phone_reduce_map, reduce_char_map) = utility.read_map('./data')
     data = utility.read_data('./data', 'mfcc', 'train')
     labels = utility.read_train_labels('./data/train.lab')
     speaker_list = utility.gen_speaker_list(phone_idx_map, params['num_steps'], data, labels)
     # (X, y) = utility.pair_data_label(raw_data, labels, phone_idx_map)
-    model_path = './model/rnn_lstm/07'
+    model_path = './model/cnn_rnn_lstm/01'
 
 
 
     # Building network
-    graph = network.build_lstm_graph(params)
+    graph = network.build_cnn_lstm_graph(params)
 
     # Training
     sess, training_losses = train_network(speaker_list, graph, params, model_path)
-
-    # Testing
-    # print('Testing...')
-    # test_data = utility.read_data('./data', 'mfcc', 'test')
-    # predict(sess, test_data, graph, params, idx_phone_map,
-    #         phone_reduce_map, reduce_char_map, './out/01.out')
--- src/network.py
+++ src/network.py
@@ -2,6 +2,103 @@
 # import numpy as np
 import tensorflow as tf
 
+def get_weight_variable(shape):
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
+
+def get_bias_variable(shape):
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
+
+def conv2d(x, W):
+     return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+# Only pooling in the feature domain
+def max_pool(x, pool_size):
+    return tf.nn.max_pool(x, ksize=[1, pool_size, pool_size, 1],
+                        strides=[1, 1, pool_size, 1], padding='SAME')
+
+def build_cnn_lstm_graph(params):
+    """
+    CNN as feature extractor for RNN (LSTM) 
+    """
+    # hyperparameters
+    feature_size = params['feature_size']
+    num_steps = params['num_steps']
+    num_classes = params['num_classes']
+    cnn_filter_size = params['cnn_filter_size']
+    cnn_filter_num = params['cnn_filter_num'] # list of int (#filter for each layer)
+    cnn_layer_num = params['cnn_layer_num']
+    cnn_pool_size = params['cnn_pool_size'] # list of int (pooling size)
+    rnn_state_size = params['rnn_state_size']
+    learning_rate = params['learning_rate']
+    # rnn_num_steps = params['rnn_num_steps']
+    assert(cnn_layer_num == len(cnn_filter_num))
+
+    # Input and output 
+    x = tf.placeholder(tf.float32, [None, num_steps, feature_size], \
+    name='cnn_input_layer')
+    y = tf.placeholder(tf.float32, [None, num_steps], \
+    name='rnn_output_layer')
+
+    # CNN layers
+    # x_4d = tf.reshape(x [None, num_steps, feature_size, 1])
+    x_4d = tf.expand_dims(x, 3)
+    # Conv. layers
+    hs = [x_4d]
+    expanded_filter_num = [1] + cnn_filter_num
+    for idx in range(cnn_layer_num):
+        input_channel = expanded_filter_num[idx]
+        output_channel = cnn_filter_num[idx]
+        weight = get_weight_variable([cnn_filter_size, cnn_filter_size, input_channel, output_channel])
+        bias = get_bias_variable([output_channel])
+        hs.append(max_pool(tf.nn.relu(conv2d(hs[-1], weight) + bias), cnn_pool_size[idx]))
+    
+    cnn_output = hs[-1]
+    # Expected shape of output (cnn_num_steps, reduced_feature_size, cnn_filter_num[-1])
+    print('CNN output. Tensor shape = ', tf.shape(cnn_output))
+
+    # Swap feature dimension and the channel dimension
+    rnn_input = tf.transpose(cnn_output, perm=[0, 2, 3, 1])
+    print('RNN input. Tensor shape = ', tf.shape(rnn_input))
+    
+    # RNN LSTM model
+    input_dims = tf.shape(rnn_input)
+    cell = tf.nn.rnn_cell.LSTMCell(rnn_state_size, state_is_tuple=True)
+    rnn_init_state = cell.zero_state(input_dims[0], tf.float32)
+    rnn_outputs, rnn_final_state = tf.nn.dynamic_rnn(cell, rnn_input, init_state=rnn_init_state)
+
+    w_rnn_out = get_weight_variable([rnn_state_size, num_classes])
+    b_rnn_out = get_bias_variable([num_classes])
+
+    rnn_outputs = tf.reshape(rnn_outputs, [-1, rnn_state_size])
+    logits = tf.matmul(rnn_outputs, w_rnn_out) + b_rnn_out
+    logits = tf.reshape(logits, input_dims[0:2] + [num_classes])
+
+    last_output = tf.slice(logits, [0, input_dims[1]-1, 0], [input_dims[0], 1, num_classes])
+    predictions_one_hot = tf.nn.softmax(tf.squeeze(last_output))
+    predictions = tf.argmax(predictions_one_hot, axis=1)
+
+    total_loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=y))
+    train_step = tf.train.AdamOptimizer(learning_rate).minimize(total_loss)
+
+    # Add tensors to collection for model saving
+    tf.add_to_collection('predictions', predictions)
+    tf.add_to_collection('train_step', train_step)
+    tf.add_to_collection('total_loss', total_loss)
+    tf.add_to_collection('x', x)
+    tf.add_to_collection('y', y)
+
+    return dict(
+        x = x,
+        y = y,
+        rnn_init_state = rnn_init_state,
+        rnn_final_state = rnn_final_state,
+        total_loss = total_loss,
+        train_step = train_step,
+        predictions = predictions
+    )
+
 
 def build_lstm_graph(params):
     """
@@ -23,7 +120,7 @@ def build_lstm_graph(params):
 
     batch_size = tf.shape(x)[0]
     # Coding ouput by one-hot encoding
-    y_one_hot = tf.one_hot(y, num_classes, dtype=tf.int32)
+    # y_one_hot = tf.one_hot(y, num_classes, dtype=tf.int32)
 
     cell = tf.nn.rnn_cell.LSTMCell(state_size, state_is_tuple=True)
     init_state = cell.zero_state(batch_size, tf.float32)
@@ -59,3 +156,4 @@ def build_lstm_graph(params):
         train_step = train_step,
         predictions = predictions
     )
+
